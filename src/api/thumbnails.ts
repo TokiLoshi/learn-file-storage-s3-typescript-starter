7c44fd0f35d41b6c5cc00507b435a9c5fac3d6c1
TokiLoshi <103384402+TokiLoshi@users.noreply.github.com>
2025-10-08 12:30:09 -0700

    adding encoding

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,53 +1,56 @@
 import { getBearerToken, validateJWT } from "../auth";
 import { respondWithJSON } from "./json";
-import { getVideo } from "../db/videos";
+import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
-import { BadRequestError, NotFoundError } from "./errors";
+import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
 
-type Thumbnail = {
-  data: ArrayBuffer;
-  mediaType: string;
-};
-
-const videoThumbnails: Map<string, Thumbnail> = new Map();
-
-export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
-  const { videoId } = req.params as { videoId?: string };
-  if (!videoId) {
-    throw new BadRequestError("Invalid video ID");
-  }
-
-  const video = getVideo(cfg.db, videoId);
-  if (!video) {
-    throw new NotFoundError("Couldn't find video");
-  }
-
-  const thumbnail = videoThumbnails.get(videoId);
-  if (!thumbnail) {
-    throw new NotFoundError("Thumbnail not found");
-  }
-
-  return new Response(thumbnail.data, {
-    headers: {
-      "Content-Type": thumbnail.mediaType,
-      "Cache-Control": "no-store",
-    },
-  });
-}
+const MAX_UPLOAD_SIZE = 10 << 20;
 
 export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
-  const { videoId } = req.params as { videoId?: string };
-  if (!videoId) {
-    throw new BadRequestError("Invalid video ID");
-  }
+	const { videoId } = req.params as { videoId?: string };
+	if (!videoId) {
+		throw new BadRequestError("Invalid video ID");
+	}
 
-  const token = getBearerToken(req.headers);
-  const userID = validateJWT(token, cfg.jwtSecret);
+	const token = getBearerToken(req.headers);
+	const userID = validateJWT(token, cfg.jwtSecret);
 
-  console.log("uploading thumbnail for video", videoId, "by user", userID);
+	const video = getVideo(cfg.db, videoId);
+	if (!video) {
+		throw new NotFoundError("Video not found");
+	}
+	if (video.userID !== userID) {
+		throw new UserForbiddenError("Unauthorized");
+	}
 
-  // TODO: implement the upload here
+	// TODO: implement the upload here
+	const formData = await req.formData();
+	const file = formData.get("thumbnail");
+	if (!(file instanceof File)) {
+		throw new BadRequestError("Thumbnail file missing");
+	}
 
-  return respondWithJSON(200, null);
+	if (file.size > MAX_UPLOAD_SIZE) {
+		throw new BadRequestError("File is too big");
+	}
+
+	const mediaType = file.type;
+	if (!mediaType) {
+		throw new BadRequestError("Missing Content-Type");
+	}
+
+	const data = await file.arrayBuffer();
+	const buffer = Buffer.from(data);
+	const base64String = buffer.toString("base64");
+	const dataURL = `data:${mediaType};base64,${base64String}`;
+
+	const thumbnailUrl = `http://localhost:8091/api/thumbnails/${videoId}`;
+
+	video.thumbnailURL = dataURL;
+
+	const updatedVideo = updateVideo(cfg.db, video);
+	console.log("Updated video: ", updatedVideo);
+
+	return respondWithJSON(200, video);
 }
